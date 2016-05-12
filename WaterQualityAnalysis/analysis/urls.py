6cd3633a19e4fe1f2e1d684544d9444094d2b8f2
dollygupta <dgupta248@gmail.com>
2016-05-11 19:16:00 -0700

    changed scripts location

--- WaterQualityAnalysis/analysis/urls.py
+++ WaterQualityAnalysis/analysis/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [ url(r'^$', views.index, name='index'),
                 url(r'^analysis', views.cluster, name='cluster'),
                 url(r'^spider', views.spider, name='spider'),
                 url(r'^qualityTrends', views.qualityTrends, name='qT'),
-                url(r'^getSensorData', views.getSensorData, name='sensorData'),]
+                url(r'^getSensorData', views.getSensorData, name='sensorData'),
+                url(r'^do_index.html', views.do_index, name='spider'),
+                url(r'^ph_index.html', views.ph_index, name='spider'),
+                url(r'^salinity_index.html', views.salinity_index, name='spider'),
+                url(r'^turbidity_index.html', views.turbidity_index, name='spider'),
+                url(r'^watertemp_index.html', views.water_index, name='spider'),
+                url(r'^chl_index.html', views.spider, name='spider'),
+               ]
