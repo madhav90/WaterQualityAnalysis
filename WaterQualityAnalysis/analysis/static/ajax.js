a9eeb6531a3a6da7358b2e7aa02af4364088ce8b
dollygupta <dgupta248@gmail.com>
2016-05-31 13:09:27 -0700

    final commit

--- WaterQualityAnalysis/analysis/static/ajax.js
+++ WaterQualityAnalysis/analysis/static/ajax.js
@@ -16,3 +16,22 @@ function getData(stationName)
 		}
 	});
 }
+
+function getData(stationName)
+{
+	//make db connection and get latlng and show pointers
+	$.ajax({
+		url : '127.0.0.1:8000/spider',
+		type : 'GET',
+		data :{
+			"station" : stationName,
+		},
+		dataType: 'json',
+		success : function(data) {
+				console.log("success in data:"+ data);
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			console.log('error here' + textStatus + "" + errorThrown);
+		}
+	});
+}
